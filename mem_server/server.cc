51ee037914a9f4c23b72a0be6e3fac597d6d49cd
Dimitris Lekkas <dlekkasp@gmail.com>
2021-09-09 21:45:29 +0200

    first impl effort of dummy memory server

---
+++ mem_server/server.cc
@@ -0,0 +1,118 @@
+#include <stdio.h>
+
+#include <rdma/rdma_cma.h>
+
+struct device {
+	struct ibv_pd *pd;
+	struct ibv_context *ctx;
+};
+
+
+struct ctrl {
+	struct ibv_pd *pd;
+	struct ibv_context *ctx;
+	struct rdma_cm_id *cm_id;
+	struct ibv_qp *qp;
+	struct ibv_cq *cq;
+
+	struct ibv_mr *mr;
+	void *buf;
+};
+
+int on_connect_request(struct rdma_cm_id *cm_id, struct rdma_conn_param *param) {
+	int ret;
+	struct ibv_qp_init_attr attr;
+	struct rdma_conn_param conn_param;
+
+	memset(&attr, 0, sizeof(struct ibv_qp_init_attr));
+	// attr.send_cq = ??
+	// attr.recv_cq = ??
+	attr.cap.max_send_wr = 1;
+	attr.cap.max_recv_wr = 1;
+	attr.cap.max_send_sge = 1;
+	attr.cap.max_recv_sge = 1;
+	attr.cap.max_inline_data = 0;
+	attr.qp_type = IBV_QPT_RC;
+
+	memset(&conn_param, 0, sizeof(conn_param));
+	conn_param.private_data = NULL;
+	conn_param.private_data_len = 0;
+	conn_param.responder_resources = param->responder_resources;
+	conn_param.initiator_depth = param->initiator_depth;
+	conn_param.flow_control = param->flow_control;
+	conn_param.rnr_retry_count = param->rnr_retry_count;
+
+
+
+	ret = rdma_create_qp
+}
+
+int cm_event_handler(struct rdma_cm_id *cm_id, struct rdma_cm_event *event) {
+	switch (event->event) {
+		case RDMA_CM_EVENT_CONNECT_REQUEST:
+			// Upon receiving a REQ message, we can allocate a QP and accept the
+			// incoming connection request.
+			on_connect_request(cm_id);
+			printf("Received a connection request.\n");
+			break;
+		case RDMA_CM_EVENT_ESTABLISHED:
+			// Upon receiving a message that connection is established, we can
+			// acknowledge the event back to the client. The call below also frees
+			// the event structure and any memory it references.
+			printf("Connection is established successfully.\n");
+			break;
+	}
+}
+
+
+int main(void) {
+	int ret;
+	struct rdma_event_channel* ev_channel;
+	ev_channel = rdma_create_event_channel();
+	if (!ev_channel) {
+		printf("Failed to create event channel.\n");
+		return -1;
+	}
+
+	struct rdma_cm_id *server_cm_id;
+	ret = rdma_create_id(ev_channel, &server_cm_id, NULL, RDMA_PS_TCP);
+	if (ret) {
+		printf("rdma_create_id() failed with exit code %d.\n", ret);
+		return ret;
+	}
+
+	struct sockaddr_in sa;
+	sa.sin_family = AF_INET;
+	sa.sin_port = 10000;
+	ret = inet_pton(AF_INET, "192.168.1.10", &(sa.sin_addr));
+	if (ret) {
+		printf("invalid IP address.\n");
+		return ret;
+	}
+
+	// bind the RDMA identifier to the source address and port
+	ret = rdma_bind_addr(server_cm_id, (struct sockaddr *) &sa);
+	if (ret) {
+		printf("rdma_bind_addr() failed with exit code %d.\n", ret);
+		return ret;
+	}
+
+	// Listen for incoming connection requests.
+	// The 2nd argument of rdma_listen dictates how many pending rdma
+	// connect requests the kernel will keep in-queue for the application
+	// to accept/reject.
+	ret = rdma_listen(server_cm_id, 2);
+	if (ret) {
+		printf("rdma_listen() failed with exit code %d.\n", ret);
+		return ret;
+	}
+
+	struct rdma_cm_event *event;
+	while (rdma_get_cm_event(ev_channel, &event) == 0) {
+		on_event(server_cm_id, event);
+		rdma_ack_cm_event(event);
+	}
+
+
+	rdma_destroy_event_channel(ev_channel);
+}
