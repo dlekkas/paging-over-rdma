e6ddfa0aaa36f9dfddddc20f4215a38b66fa6d52
Dimitris Lekkas <dlekkasp@gmail.com>
2021-11-13 20:12:04 +0100

    added benchmark app with page strided accesses

---
+++ benchmarks/stress/example_strided.cc
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <string>
+
+// computation that invokes swapping activity
+int dummy_computation(std::vector<int>& v) {
+	auto cnt = 0;
+	for (size_t i = 0; i < v.size(); i+=4096) {
+		v[i] = i / 4096;
+	}
+	return cnt;
+}
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <num>\n";
+		std::exit(1);
+	}
+	int val = std::stoi(argv[1]);
+	int ws_sz = val * 1024 * 4096;
+	std::vector<int> vec(ws_sz);
+
+	auto t1 = std::chrono::high_resolution_clock::now();
+	dummy_computation(vec);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	auto elapsed_ms = std::chrono::duration_cast<
+		std::chrono::milliseconds>(t2 - t1).count();
+	std::cout << elapsed_ms << std::endl;
+}
